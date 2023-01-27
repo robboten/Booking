5fd77c7bbc0b3e64e7aee132ef7e80698725356d
robboten <robboten@gmail.com>
2023-01-27 10:21:37 +0100

    TimeOfRegistration

--- Booking.Data/Data/ApplicationDbContext.cs
+++ Booking.Data/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Booking.Core.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 using IdentityRole = Microsoft.AspNetCore.Identity.IdentityRole;
 
 namespace Booking.Data.Data
@@ -19,6 +20,7 @@ namespace Booking.Data.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+
             //string ADMIN_ID = "02174cf0–9412–4cfe-afbf-59f706d72cf6";
             //string ROLE_ID = "341743f0-asd2–42de-afbf-59kmkkmk72cf6";
 
@@ -56,9 +58,20 @@ namespace Booking.Data.Data
             //    RoleId = ROLE_ID,
             //    UserId = ADMIN_ID
             //});
-
+            builder.Entity<ApplicationUser>().Property<DateTime>("TimeOfRegistration");
             base.OnModelCreating(builder);
             builder.Entity<ApplicationUserGymClass>().HasKey(a => new { a.ApplicationUserId, a.GymClassId });
+
+
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<ApplicationUser>().Where(e => e.State == EntityState.Added))
+            {
+                entry.Property("TimeOfRegistration").CurrentValue = DateTime.Now;
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
